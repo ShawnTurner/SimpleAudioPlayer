23dbb4bbeeca7945ce8a5235b0985fe2ad17d632
Shawn Turner <shawn.turner@gmail.com>
2017-02-11 10:21:29 -0500

    fix bug in doc title set

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8" />    
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-    <title>$title</title>
+    <title><?=$title?></title>
 	<!-- HTML attributed to http://www.script-tutorials.com/html5-audio-player-with-playlist/ -->
     <!-- add styles and scripts -->
     <link href="css/styles.css" rel="stylesheet" type="text/css" />
